ee821d4e4f2f975af9dd369c321c3bbebceee17d
Alecoow <alecoow@gmail.com>
2024-09-22 23:29:57 -0700

    implement FilterAnswer and CompareAnswer

--- main.c
+++ main.c
@@ -12,24 +12,37 @@ void GenerateList(int size, int* list) {
 
 char* FilterAnswer(char input[], int size) {
 
-    const char* trunc = strtok(input, ",\n");
-    char* new_string = malloc(size * sizeof(char));
+    const char* trunc = strtok(input, ", \n");
+    char* new_string = malloc((size+1) * sizeof(char));
     new_string[0] = '\0';
     while (trunc != NULL) {
         strcat(new_string, trunc);
-        trunc = strtok(NULL, ",\n");
+        trunc = strtok(NULL, ", \n");
     }
-    char* output = new_string;
-    free(new_string);
-    return output;
+    return new_string;
+
 
 }
 
-bool CompareAnswer(char* input, int* answer, int size_of_input, int size_of_answer) {
-    //convert string input to integer input, then compare to answer
+bool CompareAnswer(char* input, const int* answer, int size_of_input, int size_of_answer) {
     char* filtered = FilterAnswer(input, size_of_input);
-    //for (int i = 0; i <) {}
-    return true;
+    int* input_numbers = malloc((size_of_answer+1) * sizeof(int));
+
+    for (int i = 0; i < size_of_answer; i++) {
+        input_numbers[i] = filtered[i] - '0';
+    }
+
+    bool result = true;
+    for (int i = 0; i < size_of_answer; i++) {
+        if (input_numbers[i] != answer[i]) {
+            result = false;
+            break;
+        }
+    }
+
+    free(filtered);
+    free(input_numbers);
+    return result;
 }
 
 int** GenerateMap(const int size) {
@@ -49,7 +62,7 @@ void FreeMap(int** map, const int size) {
     }
 }
 
-enum POWERUPS {
+enum POWERUPS { //TODO: implement
     POWERUP_REVEAL = 0,
     POWERUP_RESHUFFLE = 0 << 1,
     POWERUP_DOUBLE_POINTS = 0 << 2,
@@ -59,7 +72,9 @@ int main(void) {
 
     srand(time(NULL));
     for (int level = 1; level != 10; level++) {
-        int simon_numbers[level];
+        int dimensions = level % 2; // every other level should increase the dimensions of the game. Final product could have user memorize grid then pick certain numbers from it, using powerups if its too difficult
+        printf("dimensions: %d", dimensions);
+        int simon_numbers[dimensions][level];
         GenerateList(level, simon_numbers);
         for (int i = 0; i < level; i++) {
             setbuf(stdout, nullptr); // workaround for bug in Clion that prevents output while debugging
@@ -72,18 +87,16 @@ int main(void) {
 
         printf("Enter the sequence, separated by commas:\n");
 
-        char input[sizeof(int)];
-        scanf(input);
-
-        if (CompareAnswer) {
+        char input[100];
+        scanf("%99s", input);
+        if (CompareAnswer(input, simon_numbers, level, strlen(input))) {
             printf("Good job! Moving to level %d\n", level + 1);
             Sleep(2500);
             system("cls");
+        } else {
+            printf("Incorrect! You made it to level %d\n");
+            return 2;
         }
-
-
-
     }
-
     return 0;
 }
