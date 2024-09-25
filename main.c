312e7f105bdb3ad21c1ea28e6037c681616e7497
Alecoow <alecoow@gmail.com>
2024-09-24 18:43:18 -0700

    Recode program to use 2 dimensional grid (WIP)

--- main.c
+++ main.c
@@ -3,25 +3,25 @@
 #include <time.h>
 #include <windows.h>
 #define WIN32_LEAN_AND_MEAN;
+#include "main.h"
 
-void GenerateList(int size, int* list) {
-    for (int i = 0; i < size; i++) {
-        list[i] = (rand() % 10);
+void GenerateList(const int dimensions,const int size, int** list) {
+    for (int i = 0; i < dimensions; i++) {
+        for (int j = 0; j < size; j++) {
+            list[i][j] = (rand() % 10);
+        }
     }
 }
 
 char* FilterAnswer(char input[], int size) {
-
     const char* trunc = strtok(input, ", \n");
     char* new_string = malloc((size+1) * sizeof(char));
     new_string[0] = '\0';
     while (trunc != NULL) {
         strcat(new_string, trunc);
-        trunc = strtok(NULL, ", \n");
+        trunc = strtok(nullptr, ", \n");
     }
     return new_string;
-
-
 }
 
 bool CompareAnswer(char* input, const int* answer, int size_of_input, int size_of_answer) {
@@ -45,21 +45,40 @@ bool CompareAnswer(char* input, const int* answer, int size_of_input, int size_o
     return result;
 }
 
-int** GenerateMap(const int size) {
-    int** map = malloc(size * sizeof(int*));
+int* GenerateMapDimension(const int size) {
+    int* map = malloc(size * sizeof(int*));
     for (int i = 0; i < size; i++) {
         map[i] = malloc(size * sizeof(int));
-        for (int j = 0; j < size; j++) {
-            map[i][j] = (rand() % 10);
-        }
     }
     return map;
 }
 
-void FreeMap(int** map, const int size) {
-    for (int i = 0; i < size; i++) {
-        free(map[i]);
+void Flatten(void* array, int* dimensions, int current_dim, int* flat_array, int* index) {
+    if (current_dim == 1) {
+        int* arr = array;
+        for (int i = 0; i < dimensions[0]; i++) {
+            flat_array[(*index)++] = arr[i];
+        }
+    } else {
+        void** arr = array;
+        for (int i = 0; i < dimensions[0]; i++) {
+            Flatten(arr[i], dimensions + 1, current_dim - 1, flat_array, index);
+        }
     }
+}
+
+int* ReformatVector(void* array, int* dimensions, int num_dimensions) {
+    int size = 1;
+    for (int i = 0; i < num_dimensions; i++) {
+        size *= dimensions[i];
+    }
+
+    int* flat_array = malloc(size * sizeof(int));
+
+    int index = 0;
+    Flatten(array, dimensions, num_dimensions, flat_array, &index);
+
+    return flat_array;
 }
 
 enum POWERUPS { //TODO: implement
@@ -69,19 +88,17 @@ enum POWERUPS { //TODO: implement
 };
 
 int main(void) {
-
-    srand(time(NULL));
-    for (int level = 1; level != 10; level++) {
+    srand(time(nullptr));
+    for (int level = 1; level < 11; level++) {
         int dimensions = level % 2; // every other level should increase the dimensions of the game. Final product could have user memorize grid then pick certain numbers from it, using powerups if its too difficult
-        printf("dimensions: %d", dimensions);
-        int simon_numbers[dimensions][level];
-        GenerateList(level, simon_numbers);
+        int simon_numbers[level][dimensions];
+        GenerateList(dimensions, level, simon_numbers);
         for (int i = 0; i < level; i++) {
-            setbuf(stdout, nullptr); // workaround for bug in Clion that prevents output while debugging
-            printf("%d\n", simon_numbers[i]);
-            Sleep(1500);
-            system("cls");
+            for (int j = 0; j <= dimensions; j++)
+            setbuf(stdout, nullptr);
+            printf("%d\n", simon_numbers[dimensions][i]);
         }
+        Sleep(1500);
         system("cls");
         Sleep(1000);
 
