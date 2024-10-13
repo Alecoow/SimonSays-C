dc0bd2454afb74a001c548c448f401b2ccbcf510
Alecoow <alecoow@gmail.com>
2024-10-12 18:37:15 -0700

    Implement Challenges

--- main.c
+++ main.c
@@ -17,34 +17,77 @@ int main(void) {
     int dimensions = 1;
     int score = 0;
     int level = 1;
-    ClearConsole(score);
+    int score_multiplier = 1;
+    float time_multiplier = 1.0f;
+    bool extra_life = false;
+    ClearConsole(score, level);
+    int challenge = 0;
+    bool challenge_active = false;
+    // Game ends
+
+
+    printf("Welcome to Simon Says! How far can you get in 5 minutes?");
+
+    Sleep(2500);
+    ClearConsole(score, level);
+    clock_t start_time = clock();
 
     for (int difficulty = 1; difficulty < 6; difficulty++) {
+        double time_elapsed = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+        if (time_elapsed >= 300.0) {
+            printf("Time's up!\n");
+            break;
+        }
+
         if (!(difficulty % 5)) {
-            dimensions++; // every fifth level increases the dimensions of the game, while resetting the "difficulty". Final product could have user memorize grid then pick certain numbers from it, using power-ups if it's too difficult
+            dimensions++; // every fifth level increases the dimensions of the game, while resetting the "difficulty".
             difficulty = 1;
+        }
+
+        if (challenge_active) {
+            printf("Challenge Started: ");
+            switch(challenge) {
+            case DOUBLE_OR_NOTHING: // DOUBLE_OR_NOTHING
+                printf("Double or Nothing\n");
+                break;
+            case EXTRA_LIFE: // EXTRA_LIFE
+                printf("Extra Life\n");
+                extra_life = true;
+                ClearConsole(score, level);
+                break;
+            case SCORE_MULTIPLIER: // SCORE_MULTIPLIER
+                score_multiplier = (rand() % 3) + 2;
+                printf("%ix Score Multiplier\n", score_multiplier);
+                break;
+            case LIGHTNING_ROUND: // LIGHTNING_ROUND
+                printf("Lightning Round\n");
+                time_multiplier = 0.5f;
+                break;
+            default:
+                break;
+            }
+            Sleep(1000);
+            ClearConsole(score, level);
         }
 
         int** simon_numbers = AllocateArray(dimensions, difficulty);
         PopulateList(dimensions, difficulty, simon_numbers);
 
-
         for (int i = 0; i < dimensions; i++) {
             for (int j = 0; j < difficulty; j++) {
                 printf("%i  ", simon_numbers[i][j]);
-                Sleep(500);
+                Sleep((int)(500 * time_multiplier));
             }
-            Sleep(2000);
-            ClearConsole(score);
+            Sleep((int)(2000 * time_multiplier));
+            ClearConsole(score, level);
             printf("\n");
         }
-        int flattened_array[dimensions*difficulty];
+        int flattened_array[dimensions * difficulty];
         FlattenArray(simon_numbers, dimensions, difficulty, flattened_array);
-        FreeArray(simon_numbers, dimensions, difficulty);
+        FreeArray(simon_numbers, dimensions);
 
-        Sleep(1500);
-        ClearConsole(score);
-        Sleep(1000);
+        ClearConsole(score, level);
+        Sleep((int)(1000 * time_multiplier));
 
         printf("Enter the sequence, separated by commas:\n");
 
@@ -54,18 +97,36 @@ int main(void) {
         int result = CompareAnswer(buffer, flattened_array, dimensions * difficulty);
         if (result == 1) {
             printf("Good job! Moving to level %i\n", ++level);
-            Sleep(2500);
-            ClearConsole(score);
+            if (challenge == DOUBLE_OR_NOTHING) {
+                score = score * 2;
+            }
+            else {
+                score = ++score * score_multiplier;
+            }
+            if (rand() % 5 == 0) {
+                challenge = StartChallenge();
+                challenge_active = true;
+            }
         } else if (result == 0) {
-            printf("Incorrect! You made it to level %i\n", level);
-            Sleep(5000);
-            return 1;
+            printf("Incorrect! Repeating level %i\n", level); --difficulty;
+            if (extra_life) {
+                Sleep(250);
+                printf("Extra life is no longer active");
+                extra_life = false;
+            }
+            else if (challenge == 1) { // DOUBLE_OR_NOTHING
+                score = 0;
+            }
+            else {
+                score = score - (1 * score_multiplier);
+            }
         } else {
-            printf("Invalid character(s)! You made it to level %i\n", level);
-            Sleep(5000);
-            return 1;
+            printf("Invalid character(s)! Try again %i\n", level); --difficulty;
         }
-        score++;
+        Sleep(2000);
+        ClearConsole(score, level);
+        score_multiplier = 1;
     }
+    printf("Final Score: %d", score);
     return 0;
 }
--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdbool.h>
+#endif //MAIN_H
 
 int** AllocateArray(int dimensions, int difficulty) {
     int** data = malloc(sizeof(int*) * dimensions);
@@ -10,14 +10,14 @@ int** AllocateArray(int dimensions, int difficulty) {
     return data;
 }
 
-void FreeArray(int** data, int dimensions, int difficulty) {
+void FreeArray(int** data, int dimensions) {
     for (int i = 0; i < dimensions; i++) {
         free(data[i]);
     }
     free(data);
 }
 
-void ClearConsole(int score) {
+void ClearConsole(int score, int level) {
     system("cls");
     char score_buffer[24];
     sprintf_s(score_buffer, sizeof(score_buffer), "Score: %d", score);
@@ -25,7 +25,7 @@ void ClearConsole(int score) {
     printf("\x1b[0;%iH", 120-score_offset);
     printf("%s\n", score_buffer);
     printf("\x1b[0;80H");
-    printf("PowerUps: %s", "");
+    printf("Level: %d", level);
     printf("\x1b[H");
 }
 
@@ -47,6 +47,7 @@ int FilterAnswer(char* input, int size) {
     }
     int new_string_len = strlen(new_string);
     if (new_string_len > size) {
+        free(new_string);
         return 1;
     }
     for (int i = 0; i < new_string_len; i++) {
@@ -61,7 +62,6 @@ int SanitizeString(const char* string) {
         if (string[i] == '\0' || string[i] == '\n') {
             break;
         }
-
         if (!isdigit(string[i]) && string[i] != ' ' && string[i] != ',') {
             return 0;
         }
@@ -94,17 +94,16 @@ void FlattenArray(int** array, int dimensions, int elements, int* output) {
         }
     }
 }
-
-enum POWERUPS { //TODO: implement
-    POWERUP_REVEAL = 1, // activate to reveal the correct answer
-    POWERUP_RESHUFFLE = 1 << 1, // activate to replay the same level but with different numbers
-    POWERUP_DOUBLE_POINTS = 1 << 2, // activate to either twice the score if correct, or reset the score to 0
+enum CHALLENGES {
+    NO_CHALLENGE = 1 << 0,
+    DOUBLE_OR_NOTHING = 1 << 1,
+    EXTRA_LIFE = 1 << 2,
+    SCORE_MULTIPLIER = 1 << 3,
+    LIGHTNING_ROUND = 1 << 4
 };
 
-int GetRandomPowerup() {
-    int powerups[] = {POWERUP_REVEAL, POWERUP_RESHUFFLE, POWERUP_DOUBLE_POINTS};
-    int random_index = rand() % 3;
-    return powerups[random_index];
+int StartChallenge() {
+    int challenges[] = {DOUBLE_OR_NOTHING, EXTRA_LIFE, SCORE_MULTIPLIER, LIGHTNING_ROUND};
+    int random_index = rand() % 4;
+    return challenges[random_index];
 }
-
-#endif //MAIN_H
